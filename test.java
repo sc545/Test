21ad9e871cd42ae3b32a59a22d0aef26bacfaec6
Yos <sc545@mutecsoft.com>
2016-04-19 18:03:04 +0900

    master : commit

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test{
 	public static void main(String args[]){
 		System.out.println("master : first commit");
 		System.out.println("master : second commit");
+		System.out.println("master : third commit");
 	}
 }	
